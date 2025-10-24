50a213083bd65712cb3223ab2262e3241f2c8136
星觅海 <1498934815@qq.com>
2025-10-25 06:55:57 +0800

    更新 announcementConfig.ts

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,7 +2,7 @@ import type { AnnouncementConfig } from "../types/config";
 
 export const announcementConfig: AnnouncementConfig = {
   title: "公告", // 公告标题
-  content: "欢迎来到我的博客！这是一则示例公告。", // 公告内容
+  content: "欢迎来到我的博客！\n这里会分享我的日常和学习中的收集、整理及总结，希望能对你有所帮助:) 💖", // 公告内容
   closable: true, // 允许用户关闭公告
   link: {
     enable: true, // 启用链接
