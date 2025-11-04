82fc1b49f719db53899aa7c769a7be118a51def9
Arongjun <2296488511@qq.com>
2025-11-04 14:12:29 +0800

    fix: 更新友链信息，替换为阿荣博客的链接和描述

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -5,42 +5,25 @@ import type { FriendLink } from "../types/config";
 // 友链配置
 export const friendsConfig: FriendLink[] = [
   {
-    title: "夏夜流萤",
+    title: "阿荣博客",
     imgurl:
-      "https://q1.qlogo.cn/g?b=qq&nk=7618557&s=640",
+      "https://q1.qlogo.cn/g?b=qq&nk=2296488511&s=640",
     desc: "总有一场相遇，是互相喜欢的！",
-    siteurl: "https://blog.cuteleaf.cn",
-    tags: ["Blog"],
+    siteurl: "https://blog.wuenrong.com",
+    tags: ["Blog", "生活"],
     weight: 10, // 权重，数字越大排序越靠前
     enabled: true, // 是否启用
   },
   {
-    title: "Firefly Docs",
-    imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
-    desc: "Firefly主题模板文档",
-    siteurl: "https://docs-firefly.cuteleaf.cn",
-    tags: ["Docs"],
+    title: "百度",
+    imgurl: "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",
+    desc: "百度一下,你就知道",
+    siteurl: "https://www.baidu.com/",
+    tags: ["搜索"],
     weight: 9,
     enabled: true,
   },
-  {
-    title: "Firefly",
-    imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
-    desc: "Firefly 一款清新美观的 Astro 博客主题模板",
-    siteurl: "https://github.com/CuteLeaf/Firefly",
-    tags: ["GitHub", "Theme"],
-    weight: 9,
-    enabled: true,
-  },
-  {
-    title: "Astro",
-    imgurl: "https://avatars.githubusercontent.com/u/44914786?v=4&s=640",
-    desc: "The web framework for content-driven websites. ⭐️ Star to support our work!",
-    siteurl: "https://github.com/withastro/astro",
-    tags: ["Framework"],
-    weight: 8,
-    enabled: true,
-  },
+
 ];
 
 // 获取启用的友链并按权重排序
