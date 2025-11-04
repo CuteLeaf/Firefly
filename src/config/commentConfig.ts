af96eb2cfecad43c79b7538616ea73c97e3322aa
Arongjun <2296488511@qq.com>
2025-11-04 10:52:02 +0800

    fix: 修复FooterConfig中的备案号，更新commentConfig以启用Twikoo评论系统

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -3,7 +3,7 @@ import type { CommentConfig } from "../types/config";
 export const commentConfig: CommentConfig = {
   enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
   enableVisitorCount: true, // 启用文章访问量统计功能。当设置为 false 时，文章访问量统计将不会显示。需要enable和enableVisitorCount都为true时才启用。
-  type: 'none', // 当前启用的评论系统类型: none, twikoo, waline, giscus, disqus，默认为none，即不启用评论系统。
+  type: 'twikoo', // 当前启用的评论系统类型: none, twikoo, waline, giscus, disqus，默认为none，即不启用评论系统。
   //twikoo评论系统配置
   twikoo: {
     envId: "https://twikoo.wuenrong.com",
