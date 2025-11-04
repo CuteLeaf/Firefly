a68c775c51525e57fe1f1345ff761d8b0a81e292
Arongjun <2296488511@qq.com>
2025-11-04 13:18:08 +0800

    fix: 更新站点URL为新的博客地址

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://firefly.cuteleaf.cn/",
+	site: "https://blog.wuenrong.com/",
 
 	base: "/",
 	trailingSlash: "always",
