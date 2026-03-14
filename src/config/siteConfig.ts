5241486e66fabd3df82e87c221c282eac1c690f1
TimeFish <107293965+TimeFish-yu@users.noreply.github.com>
2026-03-15 00:33:07 +0800

    更新 siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -33,7 +33,7 @@ export const siteConfig: SiteConfig = {
 	// 主题色
 	themeColor: {
 		// 主题色的默认色相，范围从 0 到 360。例如：红色：0，青色：200，蓝绿色：250，粉色：345
-		hue: 165,
+		hue: 225,
 		// 是否对访问者隐藏主题色选择器
 		fixed: false,
 		// 默认模式："light" 亮色，"dark" 暗色，"system" 跟随系统
@@ -85,7 +85,7 @@ export const siteConfig: SiteConfig = {
 		// 导航菜单对齐方式，left：左对齐，center：居中
 		menuAlign: "center",
 		// 导航栏图标和标题是否跟随主题色
-		followTheme: true,
+		followTheme: false,
 	},
 
 	// 站点开始日期，用于统计运行天数
