7f28194193e205a62fbb666df2c524d01b9963d8
残颜 <3587133901@qq.com>
2026-03-22 10:28:33 +0800

    Add files via upload
    修复报错1

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -82,7 +82,7 @@ export const siteConfig: SiteConfig = {
 		// 3. 本地图片（src目录，自动优化但会增加构建时间，推荐）: { type: "image", value: "assets/images/logo.webp", alt: "Logo" }
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
-			type: "image",
+			type: "url",
 			value: "https://q2.qlogo.cn/headimg_dl?dst_uin=3585720960&spec=640",
 			alt: "🍀",
 		},
