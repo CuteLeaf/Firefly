b066505b1e0d3c72c6245039441e81ec79236801
ZSSO <1867084@qq.com>
2026-06-08 02:34:51 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ import rehypeSlug from "rehype-slug";
 import remarkDirective from "remark-directive"; /* Handle directives */
 import remarkMath from "remark-math";
 import remarkSectionize from "remark-sectionize";
-import { expressiveCodeConfig, plantumlConfig, siteConfig } from "./src/config";
+import { expressiveCodeConfig， plantumlConfig， siteConfig } from "./src/config";
 import I18nKey from "./src/i18n/i18nKey";
 import { i18n } from "./src/i18n/translation";
 import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.mjs";
@@ -85,7 +85,7 @@ export default defineConfig({
 				"#right-sidebar-dynamic",
 				"#floating-toc-wrapper",
 			],
-			smoothScrolling: false，
+			smoothScrolling: false,
 			cache: true,
 			preload: true,
 			accessibility: true,
@@ -219,7 +219,7 @@ export default defineConfig({
 				[rehypeKatex, { katex }],
 				[rehypeCallouts, { theme: siteConfig.rehypeCallouts.theme }],
 				rehypeSlug,
-				rehypeMermaid,
+				rehypeMermaid，
 				rehypePlantuml,
 				rehypeFigure,
 				[rehypeExternalLinks, { siteUrl: siteConfig.site_url }],
