947cf54774976930ae92dd9dd3033b28e15bc8b8
xyxnb123 <xyxnb123@gmail.com>
2026-07-13 11:44:00 +0800

    config:修改

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "XYX",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "自律学习｜干货整理｜经验分享",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
