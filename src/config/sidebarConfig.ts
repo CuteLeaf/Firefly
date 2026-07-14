3cce95bb61ca1e86dbee7584cee553173fe565d5
xyxnb123 <xyxnb123@gmail.com>
2026-07-14 19:46:03 +0800

    config:修改

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -49,6 +49,16 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			showOnPostPage: true,
 		},
 		{
+			// 组件类型：站点统计组件
+			type: "stats",
+			// 是否启用该组件
+			enable: true,
+			// 组件位置
+			position: "top",
+			// 是否在文章详情页显示
+			showOnPostPage: true,
+		},
+		{
 			// 组件类型：公告组件
 			type: "announcement",
 			// 是否启用该组件
@@ -105,16 +115,6 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 		{
 			// 组件类型：时间问候卡片
 			type: "timeGreeting",
-			// 是否启用该组件
-			enable: true,
-			// 组件位置
-			position: "top",
-			// 是否在文章详情页显示
-			showOnPostPage: true,
-		},
-		{
-			// 组件类型：站点统计组件
-			type: "stats",
 			// 是否启用该组件
 			enable: true,
 			// 组件位置
