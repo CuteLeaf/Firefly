c0a0c417b696945a0f76500c30b06b17c6f6bf9a
xyxnb123 <xyxnb123@gmail.com>
2026-07-14 16:59:59 +0800

    config:修改

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "study",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://xyxstudy.kdns.fr",
 
 	// 站点描述
 	description:
