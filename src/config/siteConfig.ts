01116d1299aeb2b55ea8e730bf53cf76feb4a75a
LIRH-A11Y <3040811469@qq.com>
2026-07-19 13:33:44 +0800

    更新内容

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,13 +6,13 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "TXvlog",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "TX の blog",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://www.txvlog.ccwu.cc/",
 
 	// 站点描述
 	description:
