433f8f9ceea1d176c34dc0efbf38faa3fac041f2
AEsir-Official <andyhu777888999@163.com>
2026-07-28 15:49:59 +0800

    添加了类似P3R网站的开场效果

---
+++ src/types/waterIntroConfig.ts
@@ -0,0 +1,10 @@
+export interface WaterIntroConfig {
+	enable: boolean;
+	playOncePerSession: boolean;
+	sessionStorageKey: string;
+	startDelayMs: number;
+	durationMs: number;
+	zIndex: number;
+	waterColor: string;
+	skipButtonLabel: string;
+}
