32d2e848f6f9b8a754f23b81b6eb7a268a0cea8c
Bolvin82 <tanbw7@mail2.sysu.edu.cn>
2026-09-03 18:48:43 +0800

    添加了文章下方的分享卡片、上一篇/下一篇文章导航、相关文章导航、随机推荐文章导航的开关配置

---
+++ src/types/pageDisplayConfig.ts
@@ -0,0 +1,10 @@
+export interface PageDisplayConfig {
+	/** 是否在文章底部显示分享按钮 */
+	share: boolean;
+	/** 是否在文章底部显示上一篇/下一篇按钮 */
+	postNavigation: boolean;
+	/** 是否显示相关文章卡片 */
+	relatedPosts: boolean;
+	/** 是否显示随机文章卡片 */
+	randomPosts: boolean;
+}
